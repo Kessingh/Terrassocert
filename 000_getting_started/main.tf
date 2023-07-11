0543a7086285337c2ca5774617600e52ebbb7bd4
Keshav Singh N (MINDTREE LIMITED) <v-kesinghn@microsoft.com>
2023-07-11 18:07:37 +0530

    tf cloud module

--- 000_getting_started/main.tf
+++ 000_getting_started/main.tf
@@ -1,3 +1,20 @@
+terraform {
+  cloud {
+    organization = "kessingh"
+
+    workspaces {
+      name = "getting-started"
+    }
+  }
+}
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.7.0"
+    }
+  }
+
+
 locals {
   project_name = "keshav"
 }
