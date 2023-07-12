25912f076a02095509c1e5a3b93ea7495eaf26e8
Keshav Singh N (MINDTREE LIMITED) <v-kesinghn@microsoft.com>
2023-07-12 20:03:49 +0530

    aa

--- 020_providers-azure/main.tf
+++ 020_providers-azure/main.tf
@@ -25,14 +25,14 @@ module "linuxservers" {
   depends_on = [azurerm_resource_group.example]
 }
 
-/*module "network" {
+module "network" {
   source              = "Azure/network/azurerm"
   resource_group_name = azurerm_resource_group.terraform_azure_providers.name
   subnet_prefixes     = ["10.0.1.0/24"]
   subnet_names        = ["subnet1"]
 
   depends_on = [azurerm_resource_group.terraform_azure_providers]
-}*/
+}
 
 output "linux_vm_public_name" {
   value = module.linuxservers.public_ip_dns_name
