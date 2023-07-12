5b587af7f185daa32c34a53840b9807e649235cc
Keshav Singh N (MINDTREE LIMITED) <v-kesinghn@microsoft.com>
2023-07-12 18:16:56 +0530

    removed duplicates

--- 014_provisioners-null-resource/main.tf
+++ 014_provisioners-null-resource/main.tf
@@ -72,15 +72,6 @@ data "template_file" "user_data" {
   template = file("./userdata.yaml")
 }
 
-resource "aws_key_pair" "deployer" {
-  key_name   = "deployer-key"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCqEqbJD/PVx3Vsj/L9h4lElpTP4XXG/n2NS170sEl+FgH8Ipl+uiV5rl2ja8/InBFVJrH5yYj1lSm2Ew3P5U3QbN7S4evrM14+iScCzLKljpS1aLko0gKr5YAAFxcZ5KmFi6AWDvdBwD7Y2Fm80vHAk7DgC43JqCL0x2x6AWl03q5aaUYN3NUoLaob95xJPm9njbVEUe3D276viooJJLMfjMPecLFEPLYX+wIR26qZLSY65Xpg6cUfo07bZNnYiNWK78E1NOuSU/1nnFu2wmIdyn8jSEUsEdEipGr0CU1plQk/AppBh5A+nOQcNvtenA8W8RfxiNL5iya2K8riJUEUKvD/f2bq0DEGos+meO6SjIkgKZ593yd8tLWGvZvJRIhGIt4EuFnmMlncnPIarvGywvVSCyqkKRO4NrYdewS1Irt8HeABybyhH5VOKiX42f8AyZtTlPu6hWvyJw48Og8T2wWBcjxcDE1Q2uXouV084pBoc07z9VYmdnZWoXlS3T0= ksingh@localhost.localdomain"
-}
-data "template_file" "user_data" {
-  template = file("./userdata.yaml")
-}
-
-
 resource "aws_instance" "test" {
   ami                    = "ami-0d13e3e640877b0b9"
   instance_type          = "t2.micro"
