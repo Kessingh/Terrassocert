7d1e7074adb7ed7879becbf1e9c720a0de621bbd
Keshav <ksingh@localhost.localdomain>
2023-07-14 01:37:03 +0530

    from rhel

--- 110_modules/main.tf
+++ 110_modules/main.tf
@@ -1,5 +1,5 @@
 terraform {
-  
+
 }
 
 provider "aws" {
@@ -7,13 +7,13 @@ provider "aws" {
 }
 
 module "apache" {
-  source = ".//terraform-aws-apache-example"
-  vpc_id = "vpc-0c7c01d8f590aaf57"
+  source          = ".//terraform-aws-apache-example"
+  vpc_id          = "vpc-0c7c01d8f590aaf57"
   my_ip_with_cidr = "49.37.177.252/32"
-  public_key = "${file("/home/ksingh/.ssh/id_rsa.pub")}"
-  private_key = "${file("/home/ksingh/.ssh/id_rsa")}"
-  instance_type = "t2.micro"
-  server_name = "apache-modules"
+  public_key      = file("/home/ksingh/.ssh/id_rsa.pub")
+  private_key     = file("/home/ksingh/.ssh/id_rsa")
+  instance_type   = "t2.micro"
+  server_name     = "apache-modules"
 }
 
 
