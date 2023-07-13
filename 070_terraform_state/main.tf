511e871ecb6659248a6c003a06895b6f66f829af
Keshav Singh N (MINDTREE LIMITED) <v-kesinghn@microsoft.com>
2023-07-13 19:06:59 +0530

    set up dynamic blocks

--- 070_terraform_state/main.tf
+++ 070_terraform_state/main.tf
@@ -2,14 +2,14 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "3.58.0"
+      version = "5.7.0"
     }
   }
 }
 
 provider "aws" {
   profile = "default"
-  region  = "us-east-1"
+  region  = "ap-south-1"
 }
 resource "aws_instance" "our_server" {
   ami           = "ami-087c17d1fe0178315"
