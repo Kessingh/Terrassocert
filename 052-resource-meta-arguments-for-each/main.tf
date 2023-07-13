2b758f00a2adcfe8dd8dea9a024b55212189f9db
Keshav Singh N (MINDTREE LIMITED) <v-kesinghn@microsoft.com>
2023-07-13 15:37:30 +0530

    setup foreach

--- 052-resource-meta-arguments-for-each/main.tf
+++ 052-resource-meta-arguments-for-each/main.tf
@@ -2,28 +2,29 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "3.58.0"
+      version = "5.7.0"
     }
   }
 }
 
 provider "aws" {
   profile = "default"
-  region  = "us-east-1"
+  region  = "ap-south-1"
 }
-resource "aws_instance" "my_server" {
-	for_each = {
-		nano = "t2.nano"
-		micro =  "t2.micro"
-		small =  "t2.small"
-	}
-  ami           = "ami-087c17d1fe0178315"
+resource "aws_instance" "test" {
+  ami           = "ami-0d13e3e640877b0b9"
   instance_type = each.value
-	tags = {
-		Name = "Server-${each.key}"
-	}
+  for_each = {
+    "nano" = "t2.small"
+    "micro"="t2.micro"
+    "small"="t2.small" 
+  }
+
+  tags = {
+    Name = "test-${each.key}"
+  }
 }
 
 output "public_ip" {
-  value = values(aws_instance.my_server)[*].public_ip
+  value = value(aws_instance.test)[*].public_ip
 }
