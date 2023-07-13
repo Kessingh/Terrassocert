98dba07d6d8b3f7649f5d28da2e39e7261bcb06f
Keshav Singh N (MINDTREE LIMITED) <v-kesinghn@microsoft.com>
2023-07-13 22:03:57 +0530

    added setup for resource drift correcnt ami image

--- 090_manage-resource-drift/main.tf
+++ 090_manage-resource-drift/main.tf
@@ -12,7 +12,7 @@ provider "aws" {
   region  = "ap-south-1"
 }
 resource "aws_instance" "my_server" {
-  ami           = "ami-087c17d1fe0178315"
+  ami           = "ami-0d13e3e640877b0b9"
   instance_type = "t2.micro"
 	tags = {
 		Name = "MyServer"
