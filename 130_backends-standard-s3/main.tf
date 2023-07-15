27726def11c57e7bfc287fba16478cef4d48cf59
Keshav Singh N (MINDTREE LIMITED) <v-kesinghn@microsoft.com>
2023-07-15 22:55:51 +0530

    removed workspase roles

--- 130_backends-standard-s3/main.tf
+++ 130_backends-standard-s3/main.tf
@@ -10,7 +10,7 @@ terraform {
 
 provider "aws" {
   region = "ap-south-1"
-  assume_role = "${var.workspace_iam_roles[terraform.workspace]}"
+  #assume_role = "${var.workspace_iam_roles[terraform.workspace]}"
 } 
 
 resource "aws_s3_bucket" "bucket" {
