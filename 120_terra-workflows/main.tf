af3ce8464d599459746d5b34b5ea82cf1d682928
Keshav Singh N (MINDTREE LIMITED) <v-kesinghn@microsoft.com>
2023-07-15 14:49:39 +0530

    project worked - git hub repo is private

--- 120_terra-workflows/main.tf
+++ 120_terra-workflows/main.tf
@@ -1,46 +1,96 @@
 terraform {
 
+  cloud {
+
+    organization = "kessingh"
+
+
+
+    workspaces {
+
+      name = "terra-vcs-workflow"
+
+    }
+
+}
+
 
 
 }
+
+
+
+
 
 
 
 provider "aws" {
 
+
+
   region = "ap-south-1"
+
+
 
 }
 
 
 
-module "apache" {
+resource "aws_s3_bucket" "bucket" {
 
-  source          = ".//terraform-aws-apache-example"
+	bucket = "vcs-ksingh-test-tf-workflow-1010" 
+
+}
+
+
+
+
+
+module "apache-module" {
+
+  source  = "Kessingh/apache-module/aws"
+
+  version = "1.1.0"
 
   vpc_id          = var.vpc_id
 
+
+
   my_ip_with_cidr = var.my_ip_with_cidr
+
+
 
   public_key      = var.public_key
 
-  private_key     = var.private_key
+
 
   instance_type   = var.instance_type
 
+
+
   server_name     = var.server_name
 
+
+
 }
+
+
+
+
 
 
 
 output "public_ip" {
 
-    value = module.apache.public_ip
+
+
+    value = module.apache-module.public_ip
+
+
 
   
 
+
+
 }
-
-
 
